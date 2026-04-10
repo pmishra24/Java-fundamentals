9082ce622254ef64f56a4627f649e018de46ac87
pmishra24 <46805236+pmishra24@users.noreply.github.com>
2026-04-10 23:25:04 +0100

    Day 1 - Two Sum solution

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String args[])
+{
+    int[] numbers = {2,7,11,15};
+    int target = 9;
+    HashMap<Integer, Integer> h = new HashMap<>();
+    for(int i = 0; i<numbers.length; i++)
+    {
+        int rem = target - numbers[i];
+        if(h.get(rem) != null)
+        {
+            System.out.println(numbers[i]+" "+rem);
+            break;
+        }
+        h.put(numbers[i], i+1);
+    }
+}
+}
