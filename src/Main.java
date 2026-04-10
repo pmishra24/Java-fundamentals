034e22999e776b2fba4eb880cbb9e3af85a26434
pmishra24 <46805236+pmishra24@users.noreply.github.com>
2026-04-10 23:27:09 +0100

    Day 1 - Two Sum solution

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Main
 {
-    public static void main(String args[])
+    public static void main(String[] args)
 {
     int[] numbers = {2,7,11,15};
     int target = 9;
