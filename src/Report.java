94f53f55102f10a47b99e8b64d5c330061d2cbc2
pmishra24 <46805236+pmishra24@users.noreply.github.com>
2026-04-11 15:22:04 +0100

    Day 2 - Static fields and methods - report counter and unique ID

--- src/Report.java
+++ src/Report.java
@@ -1,6 +1,8 @@
 public abstract class Report {
      private String customerName;
      private String generatedDate;
+     static int reportCounter = 0;
+     private int reportId;
 
     public void setCustomerName(String customerName) {
         if(customerName == null || customerName.isEmpty())
@@ -20,16 +22,22 @@ public abstract class Report {
         return generatedDate;
     }
 
+    public int getReportId() {
+        return reportId;
+    }
+
     public Report(String customerName, String generatedDate)
      {
          this.customerName = customerName;
          this.generatedDate = generatedDate;
+         this.reportId = ++reportCounter;
      }
 
      public void printHeader()
      {
          System.out.println("Report for " + customerName);
          System.out.println("Date created " + generatedDate);
+         System.out.println("Report Id " + reportId);
      }
 
 }
