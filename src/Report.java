658ae7e3d39a52aee1b0e495f75ed6fa8cd5a51e
pmishra24 <46805236+pmishra24@users.noreply.github.com>
2026-04-11 15:05:00 +0100

    Day 2 - Encapsulation, private fields, getters setters, input validation

--- src/Report.java
+++ src/Report.java
@@ -1,8 +1,26 @@
 public abstract class Report {
-     String customerName;
-     String generatedDate;
+     private String customerName;
+     private String generatedDate;
 
-     public Report(String customerName, String generatedDate)
+    public void setCustomerName(String customerName) {
+        if(customerName == null || customerName.isEmpty())
+            throw new IllegalArgumentException("Customer Name cannot be null");
+        this.customerName = customerName;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setGeneratedDate(String generatedDate) {
+        this.generatedDate = generatedDate;
+    }
+
+    public String getGeneratedDate() {
+        return generatedDate;
+    }
+
+    public Report(String customerName, String generatedDate)
      {
          this.customerName = customerName;
          this.generatedDate = generatedDate;
