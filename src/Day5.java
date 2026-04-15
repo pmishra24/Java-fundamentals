79cc20bfa1954791108a3d85b649f9892898ac8e
pmishra24 <46805236+pmishra24@users.noreply.github.com>
2026-04-15 01:08:12 +0100

    Day 5 - HashMap pattern - Group Anagrams

--- src/Day5.java
+++ src/Day5.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class Day5 {
     public static char firstNonRepeatingCharacter(String word){
@@ -16,6 +19,20 @@ public class Day5 {
 
         return firstCharacter;
     }
+    public static HashMap<String, ArrayList<String>> groupAnagram(ArrayList<String> words){
+        HashMap<String, ArrayList<String>> hashMap = new HashMap<>();
+        for(String word: words)
+        {
+            char[] a = word.toCharArray();
+            Arrays.sort(a);
+            String s = String.copyValueOf(a);
+            ArrayList<String> arrayList = hashMap.getOrDefault(s, new ArrayList<>());
+            arrayList.add(word);
+            hashMap.put(s, arrayList);
+        }
+        return hashMap;
+    }
+
     public static void main(String[] args){
 
         String word = "aabbcddeff";
@@ -24,6 +41,16 @@ public class Day5 {
             System.out.println("No non-repeating character found First ");
         else
             System.out.println("First non repeating character in "+ word + " is " + firstCharacter);
+
+
+        String[] words = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        HashMap<String, ArrayList<String>> hashMap = groupAnagram(new ArrayList<>(List.of(words)));
+        for(String s: hashMap.keySet()) {
+            for (String t : hashMap.getOrDefault(s, new ArrayList<>()))
+                System.out.print(t + " ");
+            System.out.println();
+        }
+
     }
 
 }
