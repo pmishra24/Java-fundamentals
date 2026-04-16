f1e2c1b65db559092a114e232ad5557affb8788d
pmishra24 <46805236+pmishra24@users.noreply.github.com>
2026-04-16 17:57:30 +0100

    Day 6 Session 2: Binary Tree and BST fundamentals

--- src/Day6.java
+++ src/Day6.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.HashMap;
 
 public class Day6 {
@@ -26,11 +27,110 @@ public class Day6 {
         return fib;
     }
 
+    public static void preorder(Node node) {
+        if (node != null)
+        {
+            System.out.print(node.getValue() + ", " );
+            preorder(node.getLeft());
+            preorder(node.getRight());
+        }
+    }
+    public static void postorder(Node node) {
+        if (node != null)
+        {
+            postorder(node.getLeft());
+            postorder(node.getRight());
+            System.out.print(node.getValue() + ", " );
+
+        }
+    }
+
+    public static void inorder(Node node) {
+        if (node != null)
+        {
+            inorder(node.getLeft());
+            System.out.print(node.getValue() + ", " );
+            inorder(node.getRight());
+
+        }
+    }
+
+    public static boolean search(Node node, int target) {
+        if(node == null) return false;
+        if(node.getValue() < target)
+            return search(node.getRight(), target);
+        else if(node.getValue() > target)
+            return search(node.getLeft(), target);
+        else
+            return true;
+    }
+
+    public static Node insert(Node node, int value) {
+        if(node == null){
+            return node = new Node(value);
+        }
+        if(node.getValue() < value){
+            node.setRight(insert(node.getRight(), value));
+        }
+        else if(node.getValue() > value){
+            node.setLeft(insert(node.getLeft(), value));
+        }
+        return node;
+    }
+
+    public static int findMin(Node node) {
+        if(node == null)  throw new IllegalArgumentException("Tree is empty");
+        while(node.getLeft() != null){
+            node = node.getLeft();
+        }
+        return node.getValue();
+    }
+
+    public static int findMax(Node node) {
+        if(node == null)  throw new IllegalArgumentException("Tree is empty");
+        while(node.getRight() != null){
+            node = node.getRight();
+        }
+        return node.getValue();
+    }
+
     public static  void main(String[] args){
         int num = 4;
         System.out.println("Factorial of " + num + " is " + factorial(num));
         long fibonacciNumber = 50;
 //        System.out.println("Fibonacci at " + fibonacciNumber + " is " + findFibonacciNumber(fibonacciNumber));
         System.out.println("Fibonacci at " + fibonacciNumber + " is " + findFibonacciNumberOptimised(fibonacciNumber));
+
+
+        Node node3 = new Node(3);
+        Node node4 = new Node(7);
+        Node node5 = new Node(20);
+        Node node1 = new Node(5, node3, node4);
+        Node node2 = new Node(15, null , node5);
+        Node root = new Node(10, node1, node2);
+        inorder(root);
+        System.out.println();
+        preorder(root);
+        System.out.println();
+        postorder(root);
+
+        int target = 22;
+        System.out.println();
+        System.out.println("Does tree contains " + target + ": " + search(root, target));
+
+        int value = 12;
+        Node n = insert(root, value);
+        if(n != null){
+            if(n.getLeft() != null && n.getLeft().getValue() == value)
+                System.out.println(value + " is inserted on left of " + n.getValue());
+            else
+                System.out.println(value + " is inserted on right of " + n.getValue());
+        }
+        else
+            System.out.println("Node already exists");
+
+        System.out.println("Minimum value in tree is " + findMin(root));
+        System.out.println("Maximum value in tree is " + findMax(root));
     }
+
 }
